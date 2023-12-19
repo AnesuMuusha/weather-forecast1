a1ee8211159d206d7b175f73c5508b02851dc051
AnesuMuusha <anesumuusha@gmail.com>
2023-12-19 07:22:42 +0200

    Created Temperature and Details

---
+++ src/components/TemperatureAndDetails.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+function TemperatureAndDetails(){
+  return (
+    <div>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>xxx</p>
+      </div>
+
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        
+        <p>nnn</p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            Real fell:
+            <span className="font-medium ml-1">feels_like°</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+
+            Humidity:
+            <span className="font-medium ml-1">12%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+   
+            Wind:
+            <span className="font-medium ml-1">3443 km/h</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <p className="font-light">
+          Rise:{" "}
+          <span className="font-medium ml-1">
+            sunrise, timezone, hh:mm a
+          </span>
+        </p>
+        <p className="font-light">|</p>
+
+        <p className="font-light">
+          Set:{" "}
+          <span className="font-medium ml-1">sunset, timezone, "hh:mm a"      </span>
+        </p>
+        <p className="font-light">|</p>
+
+        <p className="font-light">
+          High:{" "}
+          <span className="font-medium ml-1">45°</span>
+        </p>
+        <p className="font-light">|</p>
+
+      
+        <p className="font-light">
+          Low:{" "}
+          <span className="font-medium ml-1">22°</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default TemperatureAndDetails;
