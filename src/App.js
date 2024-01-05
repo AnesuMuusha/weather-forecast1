c5f5e97ff7e6ed8e4ced6451e1e5a9fb70cf1f94
AnesuMuusha <anesumuusha@gmail.com>
2024-01-05 12:43:16 +0200

    displayed date and time

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
-import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
 import ForecastDaily from './components/ForecastDaily';
 import Location from './components/location';
 import Weather from './components/Weather';
+import TimeAndLocation from './components/TimeAndLocation';
 function App() {
   const [selectedCity, setSelectedCity] = useState(null);
   return (
     <div className={`md:text-lg lg:text-xl xl:text-xl text-sm bg-blue-400 text-white mx-auto max-w-screen-md mt-4 py-3 px-5 bg-gradient-to-br  h-fit shadow-x shadow-gray-400`}>
       <Inputs />
       <TopButtons setQuery={setSelectedCity} />
-      <TimeAndLocation />
+      <TimeAndLocation/>
       <Location />
       
       {selectedCity && <Weather city={selectedCity} />}
